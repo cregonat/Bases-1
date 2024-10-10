90f2fc8fb7a800504e5c1d34b6e2f77cf81fb68d
DESKTOP-N36P41G\Usuario <cregonat@gmail.com>
2024-10-10 14:40:20 -0300

    Backup Super

--- Super.sql
+++ Super.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: Super
+-- Host:     Database: Super
 -- ------------------------------------------------------
 -- Server version	8.0.33-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -276,10 +276,6 @@ LOCK TABLES `Ventas` WRITE;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'Super'
---
-
---
 -- Dumping routines for database 'Super'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -292,4 +288,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-10 13:10:01
+-- Dump completed on 2024-10-10 14:39:48
